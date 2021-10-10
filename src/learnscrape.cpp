16df5bc4aca205032b72ea35159cf21c85f9c2b1
M1TE5H <mp3722@columbia.edu>
2021-10-10 04:05:10 +0100

    Update learnscrape.cpp

--- src/learnscrape.cpp
+++ src/learnscrape.cpp
@@ -4,7 +4,6 @@ void PrintVersion(int VersionMajor, int VersionMinor, int VersionStage) {
   printf ("learnscrape version %d.%d,%d\n", VersionMajor, VersionMinor, VersionStage);
 }
 
-int main()
-{
+int main(int argc, char **argv) {
   PrintVersion(0,0,0);
 }
